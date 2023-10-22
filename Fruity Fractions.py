73c828ba81c5a3db534aa8cd97d75c123967e530
Ixphor <130984092+Ixphor@users.noreply.github.com>
2023-10-22 18:09:51 +0800

    Add files via upload

---
+++ Fruity Fractions.py
@@ -0,0 +1,20 @@
+n = 1000
+m = 998244353
+solution_count = 0
+
+for x in range(-n, n + 1):
+    for y in range(-n, n + 1):
+        z = (2 * y - 3 * x) / 6
+        if abs(z) <= n and int(z) == z and x*y != 0:
+            solution_count += 1
+            
+for x in range(-n, n + 1):
+    for y in range(-n, n + 1):
+        if x*y*(3*x-2*y)*(x-2*y) != 0:
+            z = -2*(x**2-x*y)/(3*x-2*y)
+            if abs(z) <= n and int(z) == z:
+                solution_count += 1
+        else:
+            pass
+
+print (solution_count)
