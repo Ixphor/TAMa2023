e731e16946c0fd254a960b51f696a6b23ee41499
Ixphor <130984092+Ixphor@users.noreply.github.com>
2023-10-29 16:12:48 +0800

    Update Glass Maze.py

--- Glass Maze.py
+++ Glass Maze.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 def optimize(val, power):
     result = pow(val, power//2)
@@ -9,12 +10,22 @@ def optimize(val, power):
 
 n = 4
 b = [[0 for i in range(n)] for j in range(n)]
+a = [0 for i in range(n**2)]
+sum = 0
 
 for i in range(0, n):
     for j in range(0, n):
         exp = 2023*(i+1)*(j+1)
         ans = (optimize(1669661,exp))%998244353
         b[j][i] = ans
+        a[4*j + i] = ans
         
 for row in b:
     print (*row)
+    
+a.sort(reverse=False)
+
+mincount = a.count(a[0])
+sum = a[0]*(n**2-1)*mincount
+
+print(sum)
